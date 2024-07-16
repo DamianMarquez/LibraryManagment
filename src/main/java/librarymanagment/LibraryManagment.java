b1c96e2a4f6f4fe52091af4c727ff2c03479eff3
ext-damimarq_meli <damian_1986_2@hotmail.com>
2024-07-16 15:16:29 -0300

    BaseInit

---
+++ src/main/java/librarymanagment/LibraryManagment.java
@@ -0,0 +1,13 @@
+package librarymanagment;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagment {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagment.class, args);
+    }
+}
